588fd19a8f5ff008d0256ee687a9f55cf65b388b
Ali <a.rezaee@gmail.com>
2021-05-07 18:06:57 +0430

    Delete resources directory

--- resources/views/showCal/day_of_week.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="col">
-    <div class="card day day-title @if($no==7) text-danger @endif ">
-        <h6 class="card-title">
-                {{$day_of_week}}
-        </h6>
-    </div>
-</div>
-
--- resources/views/showCal/day_of_week_rotate.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="col-d">
-    <div class="card day-of-week">
-    {{$day_of_week}}
-    </div>
-</div>
